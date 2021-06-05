cbf7f07ff651af9179d0571d9d23dfa8a6f7d9ad
Lucas Setubal <lucas-setubal@live.com>
2021-06-05 16:36:00 -0300

    Finalizando animações do header

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,82 +1,49 @@
 <template>
   <div class="home">
     <header class="header">
-      <h1 class="home__title" data-aos="fade-left" data-aos-delay="1500" data-aos-duration="1000">Joaquim <strong>Levy</strong></h1>
-      <h3 class="home__subtitle" data-aos="fade-left" data-aos-delay="1900" data-aos-duration="1000">O amor é como a criança, deseja tudo o que vê</h3>
+      <div class="header__animate">
+        <div class="header__animate___block"></div>
+        <div class="header__animate___block"></div>
+        <div class="header__animate___block"></div>
+      </div>
+      <h1
+        class="home__title"
+        data-aos="fade-left"
+        data-aos-delay="1500"
+        data-aos-duration="1000"
+      >
+        Joaquim <strong>Levy</strong>
+      </h1>
+      <h3
+        class="home__subtitle"
+        data-aos="fade-left"
+        data-aos-delay="1900"
+        data-aos-duration="1000"
+      >
+        O amor é como a criança, deseja tudo o que vê
+      </h3>
+      <div class="home__welcome-arrow">
+        <h3 class="home__welcome" data-aos="fade-up" data-aos-delay="2500">Bem Vindo</h3>
+        <a href="#" class="home__arrow-down">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="70px"
+            height="70px"
+            fill="#FFF"
+          >
+            <path d="M24 24H0V0h24v24z" fill="none" opacity=".87" />
+            <path
+              d="M15.88 9.29L12 13.17 8.12 9.29c-.39-.39-1.02-.39-1.41 0-.39.39-.39 1.02 0 1.41l4.59 4.59c.39.39 1.02.39 1.41 0l4.59-4.59c.39-.39.39-1.02 0-1.41-.39-.38-1.03-.39-1.42 0z"
+            />
+          </svg>
+        </a>
+      </div>
     </header>
-    <article class="art art1" data-aos="fade" data-aos-anchor-placement="top-center">
-      sei la
-    </article>
-    <article class="art art2" data-aos="fade" data-aos-anchor-placement="top-center">
-      sei la
-    </article>
-    <article class="art art3" data-aos="fade" data-aos-anchor-placement="top-center">
-      sei la
-    </article>
-    <article class="art art4" data-aos="fade" data-aos-anchor-placement="top-center">
-      sei la
-    </article>
   </div>
 </template>
 
 <script>
+import "./index.css";
 export default {};
 </script>
-
-<style scoped>
-.header {
-  background: #5a83ddb0;
-  width: 100vw;
-  height: calc(100vh - 52px);
-}
-.home__title {
-  position: absolute;
-  top: 3rem;
-  right: 2rem;
-
-  color: #fff;
-
-  font-family: 'Jura';
-  font-size: 8rem;
-  font-weight: 300;
-  text-align: right;
-  user-select: none;
-
-  z-index: 1;
-}
-.home__title strong {
-  color: #FFF;
-  font-weight: 300;
-  user-select: none;
-
-  animation: color 10s alternate infinite;
-}
-.home__subtitle {
-  position: absolute;
-  top: 12.5rem;
-  right: 2.6rem;
-
-  color: #fff;
-
-  font-size: 1.5rem;
-  font-weight: 300;
-  text-align: right;
-  letter-spacing: .087rem;
-}
-.art {
-  opacity: .5;
-  height: 500px;
-}
-.art1 {
-  background: #e0b1ff;
-}
-.art2 {
-  background: #ffb1b1;
-}
-.art3 {
-  background: #b1ffee;
-}
-.art4 {
-  background: #fffab1;
-}
-</style>
